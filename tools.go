9cd957a3a8c356d8b0767d7a5461138d08bfda1c
aarongao <aaron.gao@imagchina.com>
2020-05-09 12:06:50 +0800

    ss

--- tools.go
+++ tools.go
@@ -1,10 +1,17 @@
 package tools
 
 import (
+	"bytes"
+	"context"
+	"crypto/md5"
 	"crypto/tls"
+	"encoding/hex"
+	"errors"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"math/rand"
+	"net"
 	"net/http"
 	"reflect"
 	"strings"
@@ -31,7 +38,7 @@ func TypeOf(obj interface{}) {
 
 func CheckError(err error) {
 	if err != nil {
-		fmt.Println("[E] ", err.Error())
+		log.Println("[E] ", err.Error())
 	}
 }
 
@@ -89,4 +96,91 @@ func GetRandomString(l int) string {
 		result = append(result, bytes[r.Intn(len(bytes))])
 	}
 	return string(result)
+}
+
+type Result struct {
+	ErrCode int
+	ErrMsg  string
+	Data    interface{}
+}
+
+type ResponseSeccess struct {
+	ErrCode int         `json:"errcode"`
+	Result  interface{} `json:"result"`
+}
+
+type Page struct {
+	ErrCode    int64       `json:"errcode"`
+	Total      int64       `json:"total"`      //总条数
+	CurrPage   int64       `json:"currpage"`   //当前页数
+	TotalPages int64       `json:"totalpages"` //总页数
+	Limit      int64       `json:"limit"`      //每页条数
+	Result     interface{} `json:"result"`
+}
+
+type ResponseError struct {
+	ErrCode int    `json:"errcode"`
+	ErrMsg  string `json:"errmsg"`
+}
+
+func PrintStruct(i interface{}) {
+	var kv = make(map[string]interface{})
+	vValue := reflect.ValueOf(i)
+	vType := reflect.TypeOf(i)
+	for i := 0; i < vValue.NumField(); i++ {
+		kv[vType.Field(i).Name] = vValue.Field(i)
+	}
+	fmt.Println("获取到数据:")
+	for k, v := range kv {
+		fmt.Print(k)
+		fmt.Print(":")
+		fmt.Print(v)
+		fmt.Println()
+	}
+}
+
+func Md5(s string) string {
+	h := md5.New()
+	h.Write([]byte(s))
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+func PrintBody(req *http.Request) {
+	s, _ := ioutil.ReadAll(req.Body)
+	fmt.Println(string(s))
+}
+
+// 内网地址
+func GetNetworkIP() (string, error) {
+	addrs, err := net.InterfaceAddrs();
+	if err != nil {
+		return "", err
+	}
+	for _, addr := range addrs {
+		if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil && ipnet.IP.IsGlobalUnicast() {
+				return ipnet.IP.String(), nil
+			}
+		}
+	}
+	return "", errors.New("valid local IP not found")
+}
+
+// 外网地址
+func GetExternalIPAddr() (exip string, err error) {
+	resp, err := http.Get("http://myexternalip.com/raw")
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+	exip = string(bytes.TrimSpace(b))
+	return
+}
+func GetContext() (ctx context.Context) {
+	ctx, _ = context.WithTimeout(context.Background(), 10*time.Second)
+	return ctx
 }
